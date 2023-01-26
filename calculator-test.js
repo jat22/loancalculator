2c0a231ce2c0c546a9fa1c1bbaba51488ae5c04a
John Wood <johna613@gmail.com>
2023-01-26 15:09:41 -0500

    test file completed

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,19 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount : 300000, 
+    years : 30, 
+    rate : 6
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('1798.65');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
+  const values = {
+    amount : 100,
+    years : 2,
+    rate: 3
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('4.30');
+})
--- calculator.js
+++ calculator.js
@@ -11,6 +11,7 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+// captures current UI input
 function getCurrentUIValues() {
   return {
     amount: +(document.getElementById("loan-amount").value),
@@ -18,45 +19,32 @@ function getCurrentUIValues() {
     rate: +(document.getElementById("loan-rate").value),
   }
 }
-
-// Get the inputs from the DOM.
-// Put some default values in the inputs
-// Call a function to calculate the current monthly payment
-
-//Sets initial values
+// sets initial default inputs and runs updateMonthly
 function setupIntialValues() {
-  document.getElementById("loan-amount").value = 300000;
-  document.getElementById("loan-years").value = 30;
-  document.getElementById("loan-rate").value = .05;
+  document.getElementById("loan-amount").value = 200000;
+  document.getElementById("loan-years").value = 20;
+  document.getElementById("loan-rate").value = 5;
   updateMonthly();
 }
 
-// Get the current values from the UI
-// Update the monthly payment
+// runs calculateMonthlyPayment() with current values; returns new monthly paymnet.
 function update() {
   let monthlyPayment = calculateMonthlyPayment(getCurrentUIValues());
   return monthlyPayment
 }
 
-// Given an object of values (a value has amount, years and rate ),
-// calculate the monthly payment.  The output should be a string
-// that always has 2 decimal places.
-
-
-
+// calculates monthly payment
 function calculateMonthlyPayment(value) {
   let principle = value["amount"];
-  let interest = value["rate"] / 12;
+  let interest = (value["rate"]/100) / 12;
   let numOfPayments = value["years"] * 12;
-  let payment = (principle * interest) / (1 - (1 + interest)**-numOfPayments);
+  let payment = (principle * interest) / (1 - (1 + interest)**(-numOfPayments));
   let cleanPayment = payment.toFixed(2);
   console.log(cleanPayment)
   return cleanPayment;
 }
 
-
-// Given a string representing the monthly payment value,
-// update the UI to show the value.
+// updates the UI based on update()
 function updateMonthly() {
   document.getElementById("monthly-payment").innerText = update()
 }
